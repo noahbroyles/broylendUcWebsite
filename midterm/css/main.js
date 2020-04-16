d014d25579989f3bd182317960e4d895138276f6
Noah Broyles <noah@javamate.net>
2020-04-15 20:32:14 -0400

    I'ma working bruh

---
+++ midterm/css/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
