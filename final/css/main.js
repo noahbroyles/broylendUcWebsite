adfbc7fdb67298e8dbcd7224dab630787b38b430
Noah Broyles <noah@javamate.net>
2020-04-20 12:01:27 -0400

    Added midterm to final so I can juice it up

---
+++ final/css/main.js
@@ -0,0 +1,9 @@
+function toggleButton(x) {
+    x.classList.toggle("change");
+    var links = document.getElementById("links");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
